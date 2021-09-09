2c2ebc7cdb45a4d25cc75e816fbd0a59777e15b1
sarra <gusmi.sarra@gmail.com>
2021-09-09 07:24:33 +0100

    first

---
+++ src/components/DrinkList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./DrinkList.css";
+import { CircularProgress } from "@material-ui/core";
+
+function DrinkList({ drinkload, post }) {
+  var list;
+  if (post.length >= 1) {
+    list = (
+      <div className="list">
+        {drinkload ? (
+          <div className="progress">
+            <CircularProgress />
+          </div>
+        ) : (
+          post
+        )}
+      </div>
+    );
+  } else {
+    list = post;
+  }
+
+  return <div>{list}</div>;
+}
+
+export default DrinkList;
